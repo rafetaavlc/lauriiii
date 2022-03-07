43a40b863a25ebc6349653c3e9f37413ee0a7eb6
rafetaavlc <65158117+rafetaavlc@users.noreply.github.com>
2022-03-07 18:32:25 +0100

    Create inicio.js

---
+++ inicio.js
@@ -0,0 +1,16 @@
+function ir(){
+  
+  var c=2468;
+  var u="NAME";
+  if(document.form.password.value==c && document.form.login.value==u){
+    alert ("Wecome to Rafeta's project");
+    window.location="tabla.html";}
+  
+  else{
+    alert("Porfavor ingrese, nombre y usuario correctos. ");
+  }
+  
+  
+  
+  
+}
